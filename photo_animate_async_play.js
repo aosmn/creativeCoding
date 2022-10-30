af3d0eda80489c9498cb0d3f49468d1bf0e58acd
Alaa Osman <alaa.osmana@gmail.com>
2022-10-30 16:19:15 +0200

    animated portrait

--- photo_animate_async_play.js
+++ photo_animate_async_play.js
@@ -14,22 +14,22 @@ const shiftX = containerW - photoW;
 
 const settings = {
   dimensions: [containerW, containerH],
-  animate: true
-  // fps: 15
+  animate: true,
+  playing: !false
 };
 
 const params = {
   pixelSize: 8,
   character: '',
-  slowVel: 1,
-  fastVel: 2,
-  velY: 0.01,
+  slowVel: .5,
+  fastVel: 1,
+  velY: .25,
   minDistance: 100,
   maxDistance: 800,
   maxFont: 80,
   fontFactor: 6,
-  movementThreshold: 100,
-  velSize: 0.005
+  movementThreshold: 255,
+  velSize: 0.01
 };
 
 let imageData;
@@ -89,15 +89,7 @@ const sketch = async ({ context, width, height, frame }) => {
     dots.push(new DotAgent(x, y, r));
   }
   return ({ context, width, height }) => {
-    // Create gradient
-    var grd = context.createLinearGradient(0, 0, width, 0);
-    grd.addColorStop(0, '#111');
-    grd.addColorStop(1, '#1e1e1e');
-
-    // Fill with gradient
-    context.fillStyle = grd;
-    // context.fillRect(10, 10, 150, 80);
-    // context.fillStyle = 'black';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
     dots.forEach(dot => {
       dot.update();
@@ -124,10 +116,10 @@ class DotAgent {
     this.pos = new Vector(x, y);
     this.vel = new Vector(random.range(0, 0), random.range(0, 0));
     if (intensity > params.movementThreshold) {
-      // this.vel = new Vector(
-      //   random.range(-1 * .1, .1),
-      //   random.range(-1 * .1, .1)
-      // );
+      this.vel = new Vector(
+        random.range(-1 * 0.1, 0.1),
+        random.range(-1 * 0.1, 0.1)
+      );
     } else if (intensity > 70 && intensity <= params.movementThreshold) {
       this.vel = new Vector(
         random.range(-1 * params.slowVel, params.slowVel),
@@ -153,7 +145,7 @@ class DotAgent {
     if (Math.abs(this.vel.x) > 0.1) {
       this.velOpacity = random.range(0, 0.001);
     }
-    this.velSize = Math.abs(this.vel.x) > 0.1 ? params.velSize : 0;
+    this.velSize = 0;
     this.opacity = getOpacity(intensity);
     this.originalOpacity = getOpacity(intensity);
     this.maxy = y + random.range(params.minDistance, params.maxDistance);
@@ -204,87 +196,8 @@ class DotAgent {
   }
 
   bounce() {
-    if (this.intensity > params.movementThreshold) {
-      if (
-        this.y <= this.originalY - 1 ||
-        this.y >= this.originalY + 1 ||
-        this.x <= this.originalX - 1 ||
-        this.x >= this.originalX + 1
-      ) {
-        this.vel.y *= -1;
-        this.vel.x *= -1;
-      }
-    }
-    if (this.intensity <= params.movementThreshold) {
-      // if (this.size >= 6 || this.size<=0.6) {
-      //   this.velSize *= -1;
-      // }
-      if (
-        this.y <= this.miny ||
-        this.y >= this.maxy ||
-        this.opacity <= 0 ||
-        this.x <= this.minx ||
-        this.x >= this.maxx
-      ) {
-        this.y = this.originalY;
-        this.x = this.originalX;
-        this.opacity = this.originalOpacity;
-        this.vel = new Vector(random.range(-0.3, 0), random.range(-0.3, 0.3));
-        if (param.character === '.') this.size = getSize(intensity);
-        else this.size = ((params.pixelSize / 1.5) * this.intensity) / 255;
-      }
-    }
-    //  else {
-    //   if (
-    //     this.y <= this.originalY - 50 ||
-    //     this.y >= this.originalY + 50 ||
-    //     this.x <= this.originalX - 50 ||
-    //     this.x >= this.originalX + 50
-    //   ) {
-    //     this.y = this.originalY;
-    //     this.x = this.originalX;
-    //   }
-    // }
+    
   }
 }
 
-// const start = async () => {
-//   manager = await canvasSketch(sketch, settings);
-// };
-
-// start();
-
-// const createPane = () => {
-//   const pane = new Tweakpane.Pane();
-//   let folder;
-
-//   folder = pane.addFolder({ title: 'Settings' });
-//   folder.addInput(params, 'pixelSize', { min: 1, max: 50, step: 1 });
-//   folder.addInput(params, 'character');
-//   folder.addInput(params, 'slowVel', { min: 0, max: 5, step: .1 });
-//   folder.addInput(params, 'fastVel', { min: 0, max: 5, step: .1 });
-//   folder.addInput(params, 'velY', { min: 0, max: 5, step: .1 });
-//   // folder.addInput(params, 'minDistance', { min: 10, max: 100, step: 1 });
-//   // folder.addInput(params, 'maxDistance', { min: 101, max: 1000, step: 1 });
-//   // folder.addInput(params, 'maxFont', { min: 20, max: 50, step: .5 });
-//   // folder.addInput(params, 'fontFactor', { min: 1, max: 8, step: .5 });
-//   folder.addInput(params, 'movementThreshold', { min: 80, max: 255, step: 1 });
-
-//   // folder.addInput(params, "scaleMin", { min: 1, max: 100 });
-//   // folder.addInput(params, "scaleMax", { min: 1, max: 100 });
-
-//   // folder = pane.addFolder({ title: "Noise" });
-//   // folder.addInput(params, "freq", { min: -0.01, max: 0.01 });
-//   // folder.addInput(params, "amp", { min: 0, max: 1 });
-//   // folder.addInput(params, "animate");
-//   // folder.addInput(params, "frame", { min: 0, max: 1000 });
-
-//   pane.on('change', ev => {
-//     // console.log(ev);
-//     manager.render();
-//   });
-// };
-
-// createPane();
-//
 canvasSketch(sketch, settings);
